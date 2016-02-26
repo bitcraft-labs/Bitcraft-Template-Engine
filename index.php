7cd836f54bb59da0d857813c1ef92e6657009c4b
D3Exclusive <eduffy2@student.fitchburgstate.edu>
2016-02-26 00:48:47 -0500

    Added file structure

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+	/*
+		Project:    Bitcraft-Template-Engine
+		Lead Devs:  Joshua Nasiatka and Eugene Duffy
+		For:        Bitcraft Service Desk Framework
+		Dev Date:   Spring 2016
+		Status:     Staging; Idea Testing; Development
+
+	*/
+
+
+
+ ?>
